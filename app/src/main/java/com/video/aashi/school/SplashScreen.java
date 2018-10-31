78fa42eac158e3fc6f2f45c784da8bea89069eac
mohanarchu <43693193+mohanarchu@users.noreply.github.com>
2018-10-31 16:11:46 +0530

    EaziEd

---
+++ app/src/main/java/com/video/aashi/school/SplashScreen.java
@@ -0,0 +1,74 @@
+package com.video.aashi.school;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+    int pStatus = 0;
+    private Handler handler = new Handler();
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circuler);
+        final ProgressBar mProgress = (ProgressBar) findViewById(R.id.circularProgressbar);
+        mProgress.setProgress(0);   // Main Progress
+        mProgress.setSecondaryProgress(100); // Secondary Progress
+        mProgress.setMax(100); // Maximum Progress
+        mProgress.setProgressDrawable(drawable);
+
+      /*  ObjectAnimator animation = ObjectAnimator.ofInt(mProgress, "progress", 0, 100);
+        animation.setDuration(50000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();*/
+        tv = (TextView) findViewById(R.id.tv);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (pStatus < 100) {
+                    pStatus += 1;
+
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            mProgress.setProgress(pStatus);
+                            tv.setText(pStatus + "%");
+
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        // Just to display the progress slowly
+                        Thread.sleep(16); //thread will take approx 3 seconds to finish
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (pStatus==100)
+                {
+                    Intent i = new Intent(SplashScreen.this, Login.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    SplashScreen.this.finish();
+                    startActivity(i);
+                }
+            }
+        }).start();
+    }
+}
